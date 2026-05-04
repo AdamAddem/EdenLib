64c2f5f97b36ce8d01ad3c7a67276a1f6dffe71f
AdamAddem <32jenjen23@gmail.com>
2026-05-04 00:33:53 -0400

    removed cstr

--- macros.hpp
+++ macros.hpp
@@ -17,19 +17,16 @@
 #define eden_nonnull_args(...) [[gnu::nonnull(__VA_ARGS__)]]
 #define eden_notnullptr _Nonnull
 #define eden_return_nonnull [[gnu::returns_nonnull]]
-#define eden_cstr_arg(unsupported_soz)
 
 #elifdef __GNUG__
 #define eden_nonull_args [[gnu::nonnull]]
 #define eden_nonnull_args(...) [[gnu::nonnull(__VA_ARGS__)]]
 #define eden_notnullptr
 #define eden_return_nonnull [[gnu::returns_nonnull]]
-#define eden_cstr_arg(onebased_arg_idx) [[gnu::null_terminated_string_arg(onebased_arg_idx)]]
 
 #else
 #define eden_nonull_args
 #define eden_nonnull_args(...)
 #define eden_notnullptr
 #define eden_return_nonnull
-#define eden_cstr_arg(unsupported_soz)
 #endif
