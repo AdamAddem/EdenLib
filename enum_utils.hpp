ecaa66c1b463c9e9ddc5ce514e11163ed1099041
MadamAdamAddem <32jenjen23@gmail.com>
2026-07-03 20:09:05 -0400

    Made enum_between not dumb

--- enum_utils.hpp
+++ enum_utils.hpp
@@ -8,21 +8,22 @@ namespace eden {
 template <ExclusivityFlag lower = flags::Inclusive, ExclusivityFlag higher = flags::Inclusive, enum_c T>
 [[nodiscard]] constexpr bool
 enumBetween(T value, T lower_bound, T higher_bound) noexcept {
-  return [&]() constexpr {
-    if constexpr (std::is_same_v<lower, flags::Exclusive>)
-      return std::to_underlying(value) > std::to_underlying(lower_bound);
-    else
-      return std::to_underlying(value) >= std::to_underlying(lower_bound);
-  }()
-  and
-  [&]() constexpr {
-    if constexpr (std::is_same_v<higher, flags::Exclusive>)
-      return std::to_underlying(value) < std::to_underlying(higher_bound);
-    else
-      return std::to_underlying(value) <= std::to_underlying(higher_bound);
-  }();
-}
+  static constexpr bool lower_exclusive = std::is_same_v<lower, flags::Exclusive>;
+  static constexpr bool higher_exclusive = std::is_same_v<higher, flags::Exclusive>;
 
+  bool res;
+  if constexpr(lower_exclusive)
+    res = std::to_underlying(value) > std::to_underlying(lower_bound);
+  else
+    res = std::to_underlying(value) >= std::to_underlying(lower_bound);
+
+  if constexpr (higher_exclusive)
+    res = res & (std::to_underlying(value) < std::to_underlying(higher_bound));
+  else
+    res = res & (std::to_underlying(value) <= std::to_underlying(higher_bound));
+
+  return res;
+}
 
 template <enum_c T>
 [[nodiscard]] consteval bool
